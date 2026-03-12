bffff8a05dd709650b7a19686b48b400e38ef175
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2026-03-12 07:23:17 +0530

    Update play.go

--- internal/modules/play.go
+++ internal/modules/play.go
@@ -649,7 +649,7 @@ func playTracksAndRespond(
 			}
 
 			addedText := F(chatID, "play_added_to_queue_single", locales.Arg{
-				"index": len(r.Queue())+ 1,
+				"index": len(r.Queue()),
 				"url":      mainTrack.URL,
 				"title":    title,
 				"duration": formatDuration(mainTrack.Duration),
